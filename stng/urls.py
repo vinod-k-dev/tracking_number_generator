eb08da79a09306cd17a801b076095159c4567610
Vinod Kumar <django2python3@gmail.com>
2024-09-19 14:28:34 +0530

    some updation

--- stng/urls.py
+++ stng/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
-from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -44,3 +45,8 @@ urlpatterns = [
     path('tracking-numbers/', TrackingNumberListView.as_view(), name='tracking-number-list'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
